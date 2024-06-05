0829b3664be6bca66476e09f5eae32e0f62fd905
Farhad Khan Haadi <89342676+FARHADGULL@users.noreply.github.com>
2024-06-05 19:37:34 +0500

    Create custom_bottom_sheet.dart

---
+++ lib/widgets/custom_bottom_sheet.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class CustomBottomSheet {
+  final List<dynamic> list;
+  final int selectedIndex;
+  final void Function(int) onValueSelected;
+
+  CustomBottomSheet({
+    required this.list,
+    required this.selectedIndex,
+    required this.onValueSelected,
+  });
+
+  show() {
+    int tempIndex = 0;
+    showModalBottomSheet(
+      context: Get.context!,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(20),
+          topRight: Radius.circular(20),
+        ),
+      ),
+      builder: (BuildContext context) {
+        return Container(
+          height: 200,
+          padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CupertinoButton(
+                child: const Text(
+                  'Cancel',
+                  style: TextStyle(fontSize: 14),
+                ),
+                onPressed: () {
+                  Get.back();
+                },
+              ),
+              Expanded(
+                child: CupertinoPicker(
+                  scrollController: FixedExtentScrollController(
+                    initialItem: selectedIndex,
+                  ),
+                  itemExtent: 35,
+                  onSelectedItemChanged: (int index) {
+                    tempIndex = index;
+                  },
+                  children: List.generate(
+                    list.length,
+                    (int index) {
+                      return Center(
+                        child: Text(
+                          list[index],
+                          style: const TextStyle(
+                            fontWeight: FontWeight.w400,
+                            fontSize: 15,
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ),
+              CupertinoButton(
+                onPressed: () {
+                  onValueSelected(tempIndex);
+                  Get.back();
+                },
+                child: const Text(
+                  'Done',
+                  style: TextStyle(fontSize: 14),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
