e77756bdabb8842759b62aa50c2be0e47238ed1c
Farhad Khan Haadi <89342676+FARHADGULL@users.noreply.github.com>
2024-06-06 14:07:12 +0500

    Create contacts_model.dart

---
+++ lib/models/contacts_model.dart
@@ -0,0 +1,12 @@
+class Contact {
+  final int id;
+  final String name;
+  final String message;
+  final String imageUrl;
+
+  Contact(
+      {required this.id,
+      required this.name,
+      required this.message,
+      required this.imageUrl});
+}
