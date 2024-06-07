e62a8032c5c08fa0ddadd0ed20125b856f7a83e1
Farhad Khan Haadi <89342676+FARHADGULL@users.noreply.github.com>
2024-06-07 10:02:36 +0500

    Create api_base_helper.dart

---
+++ lib/helpers/api_base_helper.dart
@@ -0,0 +1,355 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_app/helpers/app_strings.dart';
+import 'package:flutter_app/helpers/common_function.dart';
+import 'package:flutter_app/helpers/global_variables.dart';
+import 'package:flutter_app/helpers/urls.dart';
+import 'package:http/http.dart' as http;
+
+class ApiBaseHelper {
+  final String _baseUrl = Urls.baseUrl;
+  Map<String, String> header = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer ${GlobalVariable.token}',
+  };
+
+  Future<dynamic> getMethod({required String url}) async {
+    try {
+      Uri urlValue = Uri.parse(_baseUrl + url);
+      CommonFunction.debugPrint(
+          '*********************** Request ********************************');
+      CommonFunction.debugPrint(urlValue);
+
+      http.Response response = await http
+          .get(urlValue, headers: header)
+          .timeout(const Duration(seconds: 30));
+
+      CommonFunction.debugPrint(
+          '*********************** Response ********************************');
+      CommonFunction.debugPrint(urlValue);
+      CommonFunction.debugPrint(response.body);
+      CommonFunction.debugPrint(
+          '****************************************************************************************');
+
+      Map<String, dynamic> parsedJSON = jsonDecode(response.body);
+      return parsedJSON;
+    } on SocketException catch (e) {
+      debugPrint("$e");
+      GlobalVariable.showLoader.value = false;
+      CommonFunction.showSnackBar(
+          title: 'Error', message: AppStrings.noInternetError);
+      throw AppStrings.noInternetError;
+    } on TimeoutException catch (_) {
+      GlobalVariable.showLoader.value = false;
+      CommonFunction.showSnackBar(
+          title: 'Error', message: AppStrings.timeOutException);
+      throw AppStrings.timeOutException;
+    } on FormatException catch (_) {
+      GlobalVariable.showLoader.value = false;
+      CommonFunction.showSnackBar(
+          title: 'Error', message: AppStrings.formatException);
+      throw AppStrings.formatException;
+    } catch (e) {
+      GlobalVariable.showLoader.value = false;
+      CommonFunction.showSnackBar(
+          title: 'Error', message: AppStrings.generalApiError);
+      throw e.toString();
+    }
+  }
+
+  Future<dynamic> getMethodQueryParam(
+      {required String url, Map<String, String>? params}) async {
+    try {
+      Uri urlValue = Uri.parse(_baseUrl + url).replace(queryParameters: params);
+
+      CommonFunction.debugPrint(
+          '*********************** Request ********************************');
+      CommonFunction.debugPrint(urlValue.toString());
+
+      http.Response response = await http
+          .get(urlValue, headers: header)
+          .timeout(const Duration(seconds: 30));
+
+      CommonFunction.debugPrint(
+          '*********************** Response ********************************');
+      CommonFunction.debugPrint(urlValue.toString());
+      CommonFunction.debugPrint(response.body);
+      // CommonFunction.colorConsole(
+      //     '****************************************************************************************');
+
+      Map<String, dynamic> parsedJSON = jsonDecode(response.body);
+      return parsedJSON;
+    } on SocketException catch (e) {
+      debugPrint("$e");
+      GlobalVariable.showLoader.value = false;
+      CommonFunction.showSnackBar(
+          title: 'Error', message: AppStrings.noInternetError);
+      throw AppStrings.noInternetError;
+    } on TimeoutException catch (_) {
+      GlobalVariable.showLoader.value = false;
+      CommonFunction.showSnackBar(
+          title: 'Error', message: AppStrings.timeOutException);
+      throw AppStrings.timeOutException;
+    } on FormatException catch (_) {
+      GlobalVariable.showLoader.value = false;
+      CommonFunction.showSnackBar(
+          title: 'Error', message: AppStrings.formatException);
+      throw AppStrings.formatException;
+    } catch (e) {
+      GlobalVariable.showLoader.value = false;
+      CommonFunction.showSnackBar(
+          title: 'Error', message: AppStrings.generalApiError);
+      throw e.toString();
+    }
+  }
+
+// Future<dynamic> deleteMethod({required String url}) async {
+//   try {
+//     Uri urlValue = Uri.parse(_baseUrl + url);
+//     CommonFunction.debugPrint(
+//         '*********************** Request ********************************');
+//     CommonFunction.debugPrint(urlValue);
+//
+//     http.Response response = await http
+//         .delete(urlValue, headers: header)
+//         .timeout(const Duration(seconds: 30));
+//
+//     CommonFunction.debugPrint(
+//         '*********************** Response ********************************');
+//     CommonFunction.debugPrint(urlValue);
+//     CommonFunction.debugPrint(response.body);
+//     CommonFunction.colorConsole(
+//         '****************************************************************************************');
+//
+//     Map<String, dynamic> parsedJSON = jsonDecode(response.body);
+//     return parsedJSON;
+//   } on SocketException catch (e) {
+//     debugPrint("$e");
+//     GlobalVariable.showLoader.value = false;
+//     CustomSnackBar.showSnackBar(
+//         title: 'Error', message: AppStrings.noInternetError);
+//     throw AppStrings.noInternetError;
+//   } on TimeoutException catch (_) {
+//     GlobalVariable.showLoader.value = false;
+//     CustomSnackBar.showSnackBar(
+//         title: 'Error', message: AppStrings.timeOutException);
+//     throw AppStrings.timeOutException;
+//   } on FormatException catch (_) {
+//     GlobalVariable.showLoader.value = false;
+//     CustomSnackBar.showSnackBar(
+//         title: 'Error', message: AppStrings.formatException);
+//     throw AppStrings.formatException;
+//   } catch (e) {
+//     GlobalVariable.showLoader.value = false;
+//     CustomSnackBar.showSnackBar(
+//         title: 'Error', message: AppStrings.generalApiError);
+//     throw e.toString();
+//   }
+// }
+
+  Future<dynamic> postMethod({
+    required String url,
+    Object? body,
+  }) async {
+    try {
+      if (body != null) {
+        body = jsonEncode(body);
+      }
+      Uri urlValue = Uri.parse(_baseUrl + url);
+      CommonFunction.debugPrint(
+          '*********************** Request ********************************');
+      CommonFunction.debugPrint(urlValue);
+      CommonFunction.debugPrint(body);
+
+      http.Response response = await http
+          .post(urlValue, headers: header, body: body)
+          .timeout(const Duration(seconds: 30));
+
+      CommonFunction.debugPrint(
+          '*********************** Response ********************************');
+      CommonFunction.debugPrint(urlValue);
+      CommonFunction.debugPrint(response.body);
+      CommonFunction.debugPrint(
+          '****************************************************************************************');
+
+      Map<String, dynamic> parsedJSON = jsonDecode(response.body);
+      return parsedJSON;
+    } on SocketException catch (_) {
+      GlobalVariable.showLoader.value = false;
+      CommonFunction.showSnackBar(
+          title: 'Error', message: AppStrings.noInternetError);
+      throw AppStrings.noInternetError;
+    } on TimeoutException catch (_) {
+      GlobalVariable.showLoader.value = false;
+      CommonFunction.showSnackBar(
+          title: 'Error', message: AppStrings.timeOutException);
+      throw AppStrings.timeOutException;
+    } on FormatException catch (_) {
+      GlobalVariable.showLoader.value = false;
+      CommonFunction.showSnackBar(
+          title: 'Error', message: AppStrings.formatException);
+      throw AppStrings.formatException;
+    } catch (e) {
+      GlobalVariable.showLoader.value = false;
+      CommonFunction.showSnackBar(
+          title: 'Error', message: AppStrings.generalApiError);
+      throw e.toString();
+    }
+  }
+
+  Future<dynamic> putMethod({
+    required String url,
+    Object? body,
+  }) async {
+    try {
+      if (body != null) {
+        body = jsonEncode(body);
+      }
+      Uri urlValue = Uri.parse(_baseUrl + url);
+      CommonFunction.debugPrint(
+          '*********************** Request ********************************');
+      CommonFunction.debugPrint(urlValue);
+      CommonFunction.debugPrint(body);
+
+      http.Response response = await http
+          .put(urlValue, headers: header, body: body)
+          .timeout(const Duration(seconds: 30));
+
+      CommonFunction.debugPrint(
+          '*********************** Response ********************************');
+      CommonFunction.debugPrint(urlValue);
+      CommonFunction.debugPrint(response.body);
+      CommonFunction.debugPrint(
+          '****************************************************************************************');
+
+      Map<String, dynamic> parsedJSON = jsonDecode(response.body);
+      return parsedJSON;
+    } on SocketException catch (_) {
+      GlobalVariable.showLoader.value = false;
+      CommonFunction.showSnackBar(
+          title: 'Error', message: AppStrings.noInternetError);
+      throw AppStrings.noInternetError;
+    } on TimeoutException catch (_) {
+      GlobalVariable.showLoader.value = false;
+      CommonFunction.showSnackBar(
+          title: 'Error', message: AppStrings.timeOutException);
+      throw AppStrings.timeOutException;
+    } on FormatException catch (_) {
+      GlobalVariable.showLoader.value = false;
+      CommonFunction.showSnackBar(
+          title: 'Error', message: AppStrings.formatException);
+      throw AppStrings.formatException;
+    } catch (e) {
+      GlobalVariable.showLoader.value = false;
+      CommonFunction.showSnackBar(
+          title: 'Error', message: AppStrings.generalApiError);
+      throw e.toString();
+    }
+  }
+
+  Future<dynamic> postMethodForImage({
+    required String url,
+    required List<http.MultipartFile> files,
+    required Map<String, String> fields,
+  }) async {
+    try {
+      Uri urlValue = Uri.parse(_baseUrl + url);
+      debugPrint(
+          '*********************** Request ********************************');
+      debugPrint("$urlValue");
+
+      http.MultipartRequest request = http.MultipartRequest('POST', urlValue);
+      request.headers.remove('Content-Type');
+      request.headers.addAll({
+        'Content-Type': 'multipart/form-data',
+        'Authorization': 'Bearer ${GlobalVariable.token}',
+      });
+      request.fields.addAll(fields);
+      request.files.addAll(files);
+
+      http.StreamedResponse response = await request.send();
+      Map<String, dynamic> parsedJson =
+          await jsonDecode(await response.stream.bytesToString());
+
+      debugPrint(
+          '*********************** Response ********************************');
+      debugPrint("$urlValue");
+      debugPrint(parsedJson.toString());
+      CommonFunction.debugPrint(
+          '****************************************************************************************');
+      return parsedJson;
+    } on SocketException catch (_) {
+      GlobalVariable.showLoader.value = false;
+      GlobalVariable.noInternet(true);
+      //CommonFunction.showSnackBar(titlege)(title, message: AppStrings.noInternetError);
+      throw AppStrings.noInternetError;
+    } on TimeoutException catch (_) {
+      GlobalVariable.showLoader.value = false;
+      CommonFunction.showSnackBar(
+          title: 'Error', message: AppStrings.timeOutException);
+      throw AppStrings.timeOutException;
+    } on FormatException catch (_) {
+      GlobalVariable.showLoader.value = false;
+      CommonFunction.showSnackBar(
+          title: 'Error', message: AppStrings.formatException);
+      throw AppStrings.formatException;
+    } catch (e) {
+      GlobalVariable.showLoader.value = false;
+      CommonFunction.showSnackBar(
+          title: 'Error', message: AppStrings.generalApiError);
+      throw e.toString();
+    }
+  }
+
+  Future<dynamic> putMethodForImage({
+    required String url,
+    required List<http.MultipartFile> files,
+    required Map<String, String> fields,
+  }) async {
+    try {
+      Uri urlValue = Uri.parse(_baseUrl + url);
+      debugPrint(
+          '*********************** Request ********************************');
+      debugPrint("$urlValue");
+
+      http.MultipartRequest request = http.MultipartRequest('PUT', urlValue);
+      request.headers.addAll(header);
+      request.fields.addAll(fields);
+      request.files.addAll(files);
+      http.StreamedResponse response = await request.send();
+      Map<String, dynamic> parsedJson =
+          await jsonDecode(await response.stream.bytesToString());
+
+      debugPrint(
+          '*********************** Response ********************************');
+      debugPrint("$urlValue");
+      debugPrint(parsedJson.toString());
+      CommonFunction.debugPrint(
+          '****************************************************************************************');
+      return parsedJson;
+    } on SocketException catch (_) {
+      GlobalVariable.showLoader.value = false;
+      GlobalVariable.noInternet(true);
+      //CommonFunction.showSnackBar(titlege)(title, message: AppStrings.noInternetError);
+      throw AppStrings.noInternetError;
+    } on TimeoutException catch (_) {
+      GlobalVariable.showLoader.value = false;
+      CommonFunction.showSnackBar(
+          title: 'Error', message: AppStrings.timeOutException);
+      throw AppStrings.timeOutException;
+    } on FormatException catch (_) {
+      GlobalVariable.showLoader.value = false;
+      CommonFunction.showSnackBar(
+          title: 'Error', message: AppStrings.formatException);
+      throw AppStrings.formatException;
+    } catch (e) {
+      GlobalVariable.showLoader.value = false;
+      CommonFunction.showSnackBar(
+          title: 'Error', message: AppStrings.generalApiError);
+      throw e.toString();
+    }
+  }
+}
