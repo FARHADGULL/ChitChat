771b13be0ff52fcd94936b6f755f67f055f14c63
Farhad Khan Haadi <89342676+FARHADGULL@users.noreply.github.com>
2024-06-07 10:02:39 +0500

    Create app_strings.dart

---
+++ lib/helpers/app_strings.dart
@@ -0,0 +1,21 @@
+class AppStrings {
+  AppStrings._(); // this is to prevent anyone from instantiating this object
+  static String generalApiError = 'Something went wrong';
+  static String noInternetError = 'No internet connection';
+
+  /////////////
+  static String formatException = 'server is unable to handle request';
+  static String timeOutException = 'server is taking too long to respond ';
+  static String sessionException =
+      'Your request is not authorized as your token is expired.';
+  static String resetPassword =
+      'Enter Email/No. account to reset your password';
+  static String writeComment = "Please write your comment here...";
+  static String review = "Thank you for your review";
+
+  static String productDetails =
+      'The speaker unit contains a diaphragm that is precision-grown from NAC Audio bio-cellulose, making it stiffer, lighter and stronger than regular PET speaker units, and allowing the sound-producing diaphragm to vibrate without the levels of distortion found in other speakers. \nThe speaker unit contains a diaphragm that is precision-grown from NAC Audio bio-cellulose, making it stiffer, lighter and stronger than regular PET speaker units, and allowing the sound-producing diaphragm to vibrate without the levels of distortion found in other speakers. ';
+
+  static String profileImage =
+      'https://firebasestorage.googleapis.com/v0/b/ismmart-vms.appspot.com/o/Product%2Fi1.png-1706734369568?alt=media&token=b02fced9-f9a6-47d3-8780-b33d3df18f58';
+}
