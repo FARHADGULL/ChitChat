b800da7c468c107fa3e11ea0ed18b2566ffe0ff3
Farhad Khan Haadi <ffarhadkhan980@gmail.com>
2024-06-10 14:50:04 +0500

    updated

---
+++ lib/screens/group_chat/group_chat_view.dart
@@ -0,0 +1,401 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/helpers/common_function.dart';
+import 'package:flutter_app/helpers/global_variables.dart';
+import 'package:flutter_app/screens/message_p2p/message_viewmodel.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import '../../helpers/constants.dart';
+import '../../widgets/custom_appbar.dart';
+import '../../widgets/custom_text.dart';
+import '../../widgets/bottomsheet_item.dart';
+
+class GroupChatView extends StatelessWidget {
+  GroupChatView({super.key});
+
+  final MessageViewModel messageViewModel = Get.put(MessageViewModel());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBar(),
+      body: Column(
+        children: [
+          messageBodySection(),
+          typeMessageSection(),
+        ],
+      ),
+    );
+  }
+
+  PreferredSizeWidget? appBar() {
+    return CustomAppBar(
+      containsLeading: true,
+      title: Text(
+        messageViewModel.selectedGroupName.value.toString(),
+        style: GoogleFonts.getFont(
+          'Poppins',
+          fontWeight: FontWeight.w500,
+          fontSize: 16,
+          height: 1,
+          color: Color.fromARGB(255, 1, 2, 2),
+        ),
+      ).data,
+      actions: [
+        IconButton(
+          icon: SizedBox(
+            width: 18,
+            height: 18,
+            child: SvgPicture.asset(
+              'assets/vectors/rectangle_774_x2.svg',
+            ),
+          ),
+          onPressed: () {
+            // Handle audio call
+          },
+        ),
+        IconButton(
+          icon: SizedBox(
+            width: 24,
+            height: 24,
+            child: SvgPicture.asset(
+              'assets/vectors/videoicon.svg',
+            ),
+          ),
+          onPressed: () {
+            // Handle video call
+          },
+        ),
+      ],
+    );
+  }
+
+  Widget messageBodySection() {
+    return Expanded(
+      child: Container(
+        color: AppColors.white,
+        child: Obx(() {
+          return ListView.builder(
+            controller: messageViewModel.scrollController,
+            //reverse: true,
+            itemCount: messageViewModel.groupMessages.length,
+            itemBuilder: (context, index) {
+              final message = messageViewModel.groupMessages[index];
+              //final senderName = 
+              return Column(
+                children: [
+                  //show name of the sender 
+                  if (message.isSentByMe)
+                  Container() else
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 10.0, top: 10, bottom: 5),
+                        child: Text(
+                           messageViewModel.selectedUserName.value.toString(),
+                          style: GoogleFonts.getFont(
+                                  'Poppins',
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 16,
+                                  height: 1,
+                                  color: Color.fromARGB(255, 1, 2, 2),
+                                ),
+                        ),
+                      ),
+                    )  ,
+                  Align(
+                    alignment: message.isSentByMe
+                        ? Alignment.centerRight
+                        : Alignment.centerLeft,
+                    child: Container(
+                      margin:
+                          const EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+                      padding:
+                          const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                      decoration: BoxDecoration(
+                        color: message.isSentByMe
+                            ? AppColors.navyBlue
+                            : Colors.grey[300],
+                        borderRadius: message.isSentByMe
+                            ? const BorderRadius.only(
+                                topLeft: Radius.circular(15),
+                                bottomRight: Radius.circular(15),
+                                bottomLeft: Radius.circular(15),
+                              )
+                            : const BorderRadius.only(
+                                bottomLeft: Radius.circular(20),
+                                topRight: Radius.circular(20),
+                                bottomRight: Radius.circular(20),
+                              ),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        // crossAxisAlignment: message.isSentByMe
+                        //     ? CrossAxisAlignment.end
+                        //     : CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            message.content.toString(),
+                            style: TextStyle(
+                              color: message.isSentByMe
+                                  ? AppColors.white
+                                  : AppColors.black,
+                            ),
+                          ),
+                          const SizedBox(height: 5),
+                          Text(
+                            CommonFunction.formatDateTime(DateTime.now().toString()),
+                            style: TextStyle(
+                              color: message.isSentByMe
+                                  ? AppColors.white
+                                  : AppColors.black,
+                              fontSize: 10,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              );
+            },
+          );
+        }),
+      ),
+    );
+  }
+
+  Widget typeMessageSection() {
+    return Container(
+      decoration: const BoxDecoration(
+        color: Color(0xFFFFFFFF),
+        border: Border(
+          top: BorderSide(
+            color: Color(0xFFEEFAF8),
+            width: 1,
+          ),
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(2, 20, 10, 30),
+        child: Row(
+          children: [
+            buildFileShareIcon(),
+            buildWriteYourMessage(),
+            buildCameraIcon(),
+            buildVoiceIcon(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildFileShareIcon() {
+    return IconButton(
+      icon: SizedBox(
+        width: 18,
+        height: 20,
+        child: SvgPicture.asset(
+          'assets/vectors/path_29_x2.svg',
+        ),
+      ),
+      onPressed: () {
+        showModalBottomSheet(
+          context: Get.context!,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(top: Radius.circular(50)),
+          ),
+          builder: (BuildContext context) {
+            return Column(
+              children: [
+                Center(
+                  child: Container(
+                    margin: const EdgeInsets.only(top: 10, bottom: 20),
+                    decoration: const BoxDecoration(
+                      color: Color(0xFF969696),
+                    ),
+                    child: const SizedBox(
+                      width: 36,
+                      height: 4,
+                    ),
+                  ),
+                ),
+                CustomText(
+                  title: "Share Content",
+                  style: GoogleFonts.getFont(
+                    'Poppins',
+                    fontWeight: FontWeight.w500,
+                    fontSize: 16,
+                    height: 1,
+                    color: const Color(0xFF000E08),
+                  ),
+                ),
+                BottomSheetItemRow(
+                  title: 'Camera',
+                  icon: Icons.camera_alt,
+                  isDisabled: true,
+                  onTap: () {
+                    // Handle delete
+                  },
+                ),
+                const Divider(
+                  indent: 50,
+                  color: AppColors.grey6,
+                ),
+                BottomSheetItemRow(
+                  title: "Document",
+                  subtitle: "Share your files",
+                  icon: Icons.file_copy,
+                  isDisabled: true,
+                  onTap: () {
+                    // Handle delete
+                  },
+                ),
+                const Divider(
+                  indent: 50,
+                  color: AppColors.grey6,
+                ),
+                BottomSheetItemRow(
+                  title: "Media",
+                  subtitle: "Share photos and videos",
+                  icon: Icons.browse_gallery,
+                  isDisabled: true,
+                  onTap: () {
+                    // Handle delete
+                  },
+                ),
+                const Divider(
+                  indent: 50,
+                  color: AppColors.grey6,
+                ),
+                BottomSheetItemRow(
+                  title: "Contact",
+                  subtitle: "Share your contacts",
+                  icon: Icons.person,
+                  isDisabled: true,
+                  onTap: () {
+                    // Handle delete
+                  },
+                ),
+                const Divider(
+                  indent: 50,
+                  color: AppColors.grey6,
+                ),
+                BottomSheetItemRow(
+                  title: "Location",
+                  subtitle: "Share your location",
+                  icon: Icons.location_on,
+                  isDisabled: true,
+                  onTap: () {
+                    // Handle delete
+                  },
+                ),
+              ],
+            );
+          },
+        );
+      },
+    );
+  }
+
+  Widget buildWriteYourMessage() {
+    return Expanded(
+      child: Container(
+        decoration: BoxDecoration(
+          color: const Color(0xFFF3F6F6),
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(right: 8.0, left: 8.0),
+          child: Row(
+            children: [
+              Expanded(
+                child: Container(
+                  padding: const EdgeInsets.only(right: 10),
+                  child: TextField(
+                      controller: messageViewModel.messageController,
+                      decoration: const InputDecoration(
+                        hintText: 'Write your message',
+                        hintStyle: kPoppinsTextStyle,
+                        border: InputBorder.none,
+                      ),
+                      style: kPoppinsTextStyle.copyWith(
+                        color: const Color(0xFF000000),
+                        fontSize: 16,
+                      ),
+                      onSubmitted: (value) {
+                        messageViewModel.sendMessagesToGroup(
+                    messageViewModel.messageController.text,
+                    messageViewModel.selectedGroupId.value,
+                    GlobalVariable.userId.value,
+                  );
+                        //messageViewModel.sendMessage(value, true, userId: GlobalVariable.userId.value, targetUserId:  messageViewModel.selectedUserId.value);
+                      }),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  // messageViewModel.sendMessage(
+                  //   messageViewModel.messageController.text,
+                  //   true,
+                  // );
+                },
+                child: SizedBox(
+                  width: 18,
+                  height: 20,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(4),
+                    child: SvgPicture.asset(
+                      'assets/vectors/vector_32_x2.svg',
+                      width: 18,
+                      height: 20,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildCameraIcon() {
+    return IconButton(
+      icon: SizedBox(
+        width: 25,
+        height: 20,
+        child: SvgPicture.asset(
+          'assets/vectors/camera_012_x2.svg',
+        ),
+      ),
+      onPressed: () {
+        // Handle attachment
+      },
+    );
+  }
+
+  Widget buildVoiceIcon() {
+    return IconButton(
+      icon: SizedBox(
+        width: 20,
+        height: 22,
+        child: SvgPicture.asset(
+          'assets/vectors/vector_44_x2.svg',
+        ),
+      ),
+      onPressed: () {
+        // Handle attachment
+      },
+    );
+  }
+}
+
+// Define the text style constant globally
+const TextStyle kPoppinsTextStyle = TextStyle(
+  fontFamily: 'Poppins',
+  fontWeight: FontWeight.w300,
+  fontSize: 12,
+  height: 1,
+  color: Color(0x80797C7B),
+);
---
+++ lib/screens/home/home_model.dart
@@ -0,0 +1,56 @@
+class AllUsers {
+  int? status;
+  String? token;
+  List<Users>? allUsers;
+
+  AllUsers({ this.status, this.token, this.allUsers});
+
+  AllUsers.fromJson(Map<String, dynamic> json) {
+    status = json['status'];
+    token = json['token'];
+    if (json['allUsers'] != null) {
+      allUsers =  <Users>[];
+      json['allUsers'].forEach((v) {
+        allUsers?.add( Users.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['status'] = this.status;
+    data['token'] = this.token;
+    if (this.allUsers != null) {
+      data['allUsers'] = this.allUsers?.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class Users {
+  int? id;
+  String? password;
+  String? createdAt;
+  String? email;
+  String? username;
+
+  Users({this.id, this.password, this.createdAt, this.email, this.username});
+
+  Users.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    password = json['password'];
+    createdAt = json['created_at'];
+    email = json['email'];
+    username = json['username'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['password'] = this.password;
+    data['created_at'] = this.createdAt;
+    data['email'] = this.email;
+    data['username'] = this.username;
+    return data;
+  }
+}
