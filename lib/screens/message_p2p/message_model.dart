7a08524296955049e94a622a2e147d4beda0260a
Farhad Khan Haadi <ffarhadkhan980@gmail.com>
2024-06-12 18:52:04 +0500

    Update message_model.dart

--- lib/screens/message_p2p/message_model.dart
+++ lib/screens/message_p2p/message_model.dart
@@ -4,4 +4,20 @@ class Message {
    String? createdAt;
 
   Message({required this.content, required this.isSentByMe, this.createdAt});
+
+  factory Message.fromJson(Map<String, dynamic> json) {
+    return Message(
+      content: json['content'],
+      isSentByMe: json['isSentByMe'],
+      createdAt: json['createdAt'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'content': content,
+      'isSentByMe': isSentByMe,
+      'createdAt': createdAt,
+    };
+  }
 }
